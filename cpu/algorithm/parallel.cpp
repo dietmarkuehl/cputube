8ba9b36dac81e9eb10bad43ecc8949107ea3240f
Dietmar Kühl <dietmar.kuehl@me.com>
2017-04-03 15:20:18 +0100

    added more algorithm stubs

--- cpu/algorithm/parallel.cpp
+++ cpu/algorithm/parallel.cpp
@@ -6,6 +6,7 @@
 #include <cpu/algorithm/parallel.h>
 #include <algorithm>
 #include <iostream>
+#include <type_traits>
 
 namespace CA = cpu::algorithm;
 namespace CE = cpu::execution;
@@ -26,10 +27,18 @@ namespace {
 
 template <typename R, typename F, typename... A>
 static void call(F f, A... a) {
-    expect<R> = f(a...);
-    expect<R> = f(CE::seq, a...);
-    expect<R> = f(CE::par, a...);
-    expect<R> = f(CE::par_unseq, a...);
+    if constexpr (std::is_void_v<R>) {
+        f(a...);
+        f(CE::seq, a...);
+        f(CE::par, a...);
+        f(CE::par_unseq, a...);
+    }
+    else {
+        expect<R> = f(a...);
+        expect<R> = f(CE::seq, a...);
+        expect<R> = f(CE::par, a...);
+        expect<R> = f(CE::par_unseq, a...);
+    }
 }
 
 // ----------------------------------------------------------------------------
@@ -37,13 +46,104 @@ static void call(F f, A... a) {
 int main() {
     std::vector<int> v0;
     std::vector<int> v1;
+    std::vector<int> v2;
     using iterator = std::vector<int>::iterator;
 
     call<bool>([](auto... a){ return CA::all_of(a...); },
                v0.begin(), v0.end(), [](auto){ return true; });
     call<bool>([](auto... a){ return CA::any_of(a...); },
                v0.begin(), v0.end(), [](auto){ return true; });
+    call<bool>([](auto... a){ return CA::none_of(a...); },
+               v0.begin(), v0.end(), [](auto){ return true; });
+
+    call<void>([](auto... a){ return CA::for_each(a...); },
+               v0.begin(), v0.end(), [](auto){ return true; });
+    call<iterator>([](auto... a){ return CA::for_each_n(a...); },
+                   v0.begin(), v0.end(), 0, [](auto){ return true; });
+
+    call<iterator>([](auto... a){ return CA::find(a...); },
+                   v0.begin(), v0.end(), 0);
+    call<iterator>([](auto... a){ return CA::find_if(a...); },
+                   v0.begin(), v0.end(), [](auto){ return true; });
+    call<iterator>([](auto... a){ return CA::find_if_not(a...); },
+                   v0.begin(), v0.end(), [](auto){ return true; });
+    call<iterator>([](auto... a){ return CA::find_end(a...); },
+                   v0.begin(), v0.end(), v1.begin(), v1.end());
+    call<iterator>([](auto... a){ return CA::find_end(a...); },
+                   v0.begin(), v0.end(), v1.begin(), v1.end(), [](auto, auto){ return true; });
+    call<iterator>([](auto... a){ return CA::find_first_of(a...); },
+                   v0.begin(), v0.end(), v1.begin(), v1.end());
+    call<iterator>([](auto... a){ return CA::find_first_of(a...); },
+                   v0.begin(), v0.end(), v1.begin(), v1.end(), [](auto, auto){ return true; });
+    call<iterator>([](auto... a){ return CA::adjacent_find(a...); },
+                   v0.begin(), v0.end());
+    call<iterator>([](auto... a){ return CA::adjacent_find(a...); },
+                   v0.begin(), v0.end(), [](auto, auto){ return true; });
+
+    call<std::ptrdiff_t>([](auto... a){ return CA::count(a...); },
+                         v0.begin(), v0.end(), 0);
+    call<std::ptrdiff_t>([](auto... a){ return CA::count_if(a...); },
+                         v0.begin(), v0.end(), [](auto){ return true; });
+
+    call<std::pair<iterator, iterator>>([](auto... a){ return CA::mismatch(a...); },
+                                        v0.begin(), v0.end(), v1.begin());
+    call<std::pair<iterator, iterator>>([](auto... a){ return CA::mismatch(a...); },
+                                        v0.begin(), v0.end(), v1.begin(), v1.end());
+    call<std::pair<iterator, iterator>>([](auto... a){ return CA::mismatch(a...); },
+                                        v0.begin(), v0.end(), v1.begin(), v1.end(),
+                                        [](auto, auto){ return true; });
+    call<std::pair<iterator, iterator>>([](auto... a){ return CA::mismatch(a...); },
+                                        v0.begin(), v0.end(), v1.begin(),
+                                        [](auto, auto){ return true; });
+
+    call<bool>([](auto... a){ return CA::equal(a...); },
+               v0.begin(), v0.end(), v1.begin());
+    call<bool>([](auto... a){ return CA::equal(a...); },
+               v0.begin(), v0.end(), v1.begin(), v1.end());
+    call<bool>([](auto... a){ return CA::equal(a...); },
+               v0.begin(), v0.end(), v1.begin(), v1.end(),
+               [](auto, auto){ return true; });
+    call<bool>([](auto... a){ return CA::equal(a...); },
+               v0.begin(), v0.end(), v1.begin(),
+               [](auto, auto){ return true; });
+
+    call<iterator>([](auto... a){ return CA::search(a...); },
+                   v0.begin(), v0.end(), v1.begin(), v1.end());
+    call<iterator>([](auto... a){ return CA::search(a...); },
+                   v0.begin(), v0.end(), v1.begin(), v1.end(),
+                   [](auto, auto){ return true; });
+    call<iterator>([](auto... a){ return CA::search_n(a...); },
+                   v0.begin(), v0.end(), 0, 0);
+    call<iterator>([](auto... a){ return CA::search_n(a...); },
+                   v0.begin(), v0.end(), 0, 0,
+                   [](auto, auto){ return true; });
+
+    call<iterator>([](auto... a){ return CA::copy(a...); },
+                   v0.begin(), v0.end(), v1.begin());
+    call<iterator>([](auto... a){ return CA::copy_n(a...); },
+                   v0.begin(), 0, v1.begin());
+    call<iterator>([](auto... a){ return CA::copy_if(a...); },
+                   v0.begin(), v0.end(), v1.begin(), [](auto){ return true; });
+    call<iterator>([](auto... a){ return CA::move(a...); },
+                   v0.begin(), v0.end(), v1.begin());
+
+    call<iterator>([](auto... a){ return CA::swap_ranges(a...); },
+                   v0.begin(), v0.end(), v1.begin());
+
     call<iterator>([](auto... a){ return CA::transform(a...); },
                    v0.begin(), v0.end(), v1.begin(), [](auto v){ return v; });
+    call<iterator>([](auto... a){ return CA::transform(a...); },
+                   v0.begin(), v0.end(), v1.begin(), v2.begin(),
+                   [](auto v, auto){ return v; });
+
+    call<void>([](auto... a){ return CA::replace(a...); },
+               v0.begin(), v0.end(), 0, 0);
+    call<void>([](auto... a){ return CA::replace_if(a...); },
+               v0.begin(), v0.end(), [](auto){ return true; }, 0);
+    call<iterator>([](auto... a){ return CA::replace_copy(a...); },
+                   v0.begin(), v0.end(), v1.begin(), 0, 0);
+    call<iterator>([](auto... a){ return CA::replace_copy_if(a...); },
+                   v0.begin(), v0.end(), v1.begin(), [](auto){ return true; }, 0);
+
     std::cout << "done\n";
 }
--- cpu/algorithm/parallel.h
+++ cpu/algorithm/parallel.h
@@ -67,12 +67,8 @@ namespace cpu {
         char const* policy_name<parallel_unsequenced_policy>() {
             return "par_unseq, ";
         }
-        template <typename F>
-        char const* policy_name(F) {
-            return policy_name<F>();
-        }
-        template <typename F, typename S>
-        char const* policy_name(F, S) {
+        template <typename F, typename... T>
+        char const* policy_name(F, T...) {
             return policy_name<F>();
         }
     }
@@ -101,11 +97,286 @@ namespace cpu {
                 return 0;
             }
             // ----------------------------------------------------------------
+            using std::none_of;
+            template <typename F, typename... T>
+            bool none_of(F f, T...) {
+                char const* name = cpu::execution::policy_name(f);
+                std::cout << "none_of(" << name << "fb, fe, pred)\n";
+                return 0;
+            }
+            // ----------------------------------------------------------------
+            using std::for_each;
+            template <typename F, typename... T>
+            void for_each(F f, T...) {
+                char const* name = cpu::execution::policy_name(f);
+                std::cout << "for_each(" << name << "fb, fe, fun)\n";
+            }
+            // ----------------------------------------------------------------
+            //-dk:TODO using std::for_each_n;
+            template <typename F, typename S, typename... T>
+            auto for_each_n(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "for_each_n(" << name << "fb, fe, count, fun)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::find;
+            template <typename F, typename S, typename... T>
+            auto find(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "find(" << name << "fb, fe, value)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::find_if;
+            template <typename F, typename S, typename... T>
+                auto find_if(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "find_if(" << name << "fb, fe, pred)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::find_if_not;
+            template <typename F, typename S, typename... T>
+            auto find_if_not(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "find_if_not(" << name << "fb, fe, pred)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::find_end;
+            template <typename F, typename S, typename... T>
+            auto find_end(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "find_end(" << name << "fb, fe, sb, se)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::find_first_of;
+            template <typename F, typename S, typename... T>
+            auto find_first_of(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "find_first_of(" << name << "fb, fe, sb, se)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::adjacent_find;
+            template <typename F, typename S, typename... T>
+            auto adjacent_find(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "adjacent_find(" << name << "fb, fe, sb, se)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::count;
+            template <typename F, typename... T>
+            std::ptrdiff_t count(F f, T...) {
+                char const* name = cpu::execution::policy_name(f);
+                std::cout << "count(" << name << "fb, fe, value)\n";
+                return std::ptrdiff_t();
+            }
+            // ----------------------------------------------------------------
+            using std::count_if;
+            template <typename F, typename... T>
+            std::ptrdiff_t count_if(F f, T...) {
+                char const* name = cpu::execution::policy_name(f);
+                std::cout << "count_if(" << name << "fb, fe, pred)\n";
+                return std::ptrdiff_t();
+            }
+            // ----------------------------------------------------------------
+            //-dk:TODO using std::mismatch;
+            template <typename F, typename S, typename T, typename... U>
+            auto mismatch(F f, S s, T t, U...) {
+                char const* name = cpu::execution::policy_name(f, s, t);
+                std::cout << "mismatch(" << name << "fb, fe, sb, se)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return std::make_pair(s, t);
+                }
+                else {
+                    return std::make_pair(f, s);
+                }
+            }
+            // ----------------------------------------------------------------
+            //-dk:TODO using std::equal;
+            template <typename F, typename... T>
+            bool equal(F f, T...) {
+                char const* name = cpu::execution::policy_name(f);
+                std::cout << "equal(" << name << "fb, fe, fun)\n";
+                return false;
+            }
+            // ----------------------------------------------------------------
+            using std::search;
+            template <typename F, typename S, typename... T>
+            auto search(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "search(" << name << "fb, fe, sb, se)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::search_n;
+            template <typename F, typename S, typename... T>
+            auto search_n(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "search_n(" << name << "fb, fe, n, value)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::copy;
+            template <typename F, typename S, typename... T>
+            auto copy(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "copy(" << name << "fb, fe, sb)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::copy_n;
+            template <typename F, typename S, typename... T>
+            auto copy_n(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "copy_n(" << name << "fb, n, sb)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::copy_if;
+            template <typename F, typename S, typename... T>
+            auto copy_if(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "copy_if(" << name << "fb, fe, sb, pred)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::move;
+            template <typename F, typename S, typename... T>
+            auto move(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "move(" << name << "fb, fe, sb)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::swap_ranges;
+            template <typename F, typename S, typename... T>
+            auto swap_ranges(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "swap_ranges(" << name << "fb, fe, sb, se)\n";
+                //-dk:TODO: actually returns an iterator to the second range
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
             using std::transform;
             template <typename F, typename S, typename... T>
             auto transform(F f, S s, T... ) {
                 char const* name = cpu::execution::policy_name(f, s);
                 std::cout << "transform(" << name << "fb, fe, tb, fun)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::replace;
+            template <typename F, typename... T>
+            void replace(F f, T...) {
+                char const* name = cpu::execution::policy_name(f);
+                std::cout << "replace(" << name << "fb, fe, 0, 1)\n";
+            }
+            // ----------------------------------------------------------------
+            using std::replace_if;
+            template <typename F, typename... T>
+            void replace_if(F f, T...) {
+                char const* name = cpu::execution::policy_name(f);
+                std::cout << "replace_if(" << name << "fb, fe, pred, 1)\n";
+            }
+            // ----------------------------------------------------------------
+            using std::replace_copy;
+            template <typename F, typename S, typename... T>
+            auto replace_copy(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "replace_copy(" << name << "fb, fe, sb, 0, 1)\n";
+                if constexpr (cpu::execution::is_execution_policy_v<F>) {
+                    return s;
+                }
+                else {
+                    return f;
+                }
+            }
+            // ----------------------------------------------------------------
+            using std::replace_copy_if;
+            template <typename F, typename S, typename... T>
+            auto replace_copy_if(F f, S s, T...) {
+                char const* name = cpu::execution::policy_name(f, s);
+                std::cout << "replace_copy_if(" << name << "fb, fe, sb, pred, 1)\n";
                 if constexpr (cpu::execution::is_execution_policy_v<F>) {
                     return s;
                 }
